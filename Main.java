446787fe4599893da0bc0ea6b9d1773a5dbd7b3b
alaricomv <alarico.mercado@gmail.com>
2021-11-08 17:14:57 -0600

    first commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+
+        Person person = new Person("Alarico Mercado","Vista Magna 1443",23,"5518294302");
+        person.info();
+    }
+}
---
+++ Person.java
@@ -0,0 +1,24 @@
+class Person{
+
+    public String name;
+    public String address;
+    public int age;
+    public String phone;
+
+    //Constructor
+    public Person(String name, String address, int age, String phone){
+        this.name = name;
+        this.address = address;
+        this.age = age;
+        this.phone = phone;
+    }
+
+    //Print all info
+    public void info(){
+        System.out.println("Full name: " + name + "\n"
+                + "Address: " +address+ "\n"
+                + "Age: " +age + "\n" 
+                + "Phone: " +phone + "\n");
+    }
+
+}
